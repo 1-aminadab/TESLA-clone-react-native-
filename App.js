59a17d4237ad7cf9c3a0a5c688bb9c469c9ed724
1-aminadab <4ethiochild@gmail.com>
2023-02-25 22:52:04 +0300

    tesla clone officialy started

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model 5</Text>
+          <Text style={styles.subtitle}>Starting at $49,399</Text>
+        </View>
+        
+      </View>
     </View>
   );
 }
@@ -17,4 +22,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer:{
+    width:"100%",
+    height:"100%"
+  },
+  titles:{
+    marginTop:"35%",
+    width:"100%",
+    alignItems:"center"
+  },
+  title:{
+    fontSize:40,
+    fontWeight:"600"
+  },
+  subtitle:{
+    color:"#5c5e62"
+  }
+  
 });
